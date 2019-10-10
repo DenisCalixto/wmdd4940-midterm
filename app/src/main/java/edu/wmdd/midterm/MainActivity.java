364011a0fb35f6df71ba540c4b914a2bbcda5c8e
DenisCalixto <deniscalixto@gmail.com>
2019-10-10 15:16:08 -0700

    second spinner loading

--- app/src/main/java/edu/wmdd/midterm/MainActivity.java
+++ app/src/main/java/edu/wmdd/midterm/MainActivity.java
@@ -20,76 +20,91 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
 
     private String url = "https://learn.operatoroverload.com/rental/";
+
+    TextView textContent;
 
 //    private RecyclerView mList;
 //
 //    private LinearLayoutManager linearLayoutManager;
 //    private DividerItemDecoration dividerItemDecoration;
-//    private List<Property> propertyList;
+    private List<JSONObject> propertyList;
+    String mainOptionSelected;
 //    private RecyclerView.Adapter adapter;
+
+    Spinner contentSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mList = findViewById(R.id.main_list);
+        propertyList = new ArrayList<>();
 
-//        propertyList = new ArrayList<>();
-//        adapter = new PropertyAdapter(getApplicationContext(),propertyList);
-//
-//        linearLayoutManager = new LinearLayoutManager(this);
-//        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-//        dividerItemDecoration = new DividerItemDecoration(mList.getContext(), linearLayoutManager.getOrientation());
-//
-//        mList.setHasFixedSize(true);
-//        mList.setLayoutManager(linearLayoutManager);
-//        mList.addItemDecoration(dividerItemDecoration);
-//        mList.setAdapter(adapter);
-
-//        Button fab = findViewById(R.id.buttonGet);
-////        fab.setOnClickListener(new View.OnClickListener() {
-////            @Override
-////            public void onClick(View view) {
-////                getData();
-////            }
-////        });
+        textContent = findViewById(R.id.textContent);
 
         Spinner spinner = (Spinner) findViewById(R.id.optionSpinner);
-        spinner.setOnItemSelectedListener(this);
+//        spinner.setOnItemSelectedListener(this);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                String item = parent.getItemAtPosition(pos).toString().toLowerCase();
+                mainOptionSelected = item;
+                getFirstLevelData(item);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Another interface callback
+            }
+        });
         ArrayAdapter<CharSequence> dataAdapter = ArrayAdapter.createFromResource(this,
                 R.array.spinner_options, android.R.layout.simple_spinner_item);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
+
+
+        contentSpinner = (Spinner) findViewById(R.id.contentSpinner);
+        contentSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                String item = parent.getItemAtPosition(pos).toString().toLowerCase();
+                //getSecondLevelData(mainOptionSelected, item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Another interface callback
+            }
+        });
+
+        ArrayAdapter<JSONObject> contentDataAdapter = new ArrayAdapter<JSONObject>(this, android.R.layout.simple_spinner_item, propertyList);
+        contentDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        contentSpinner.setAdapter(contentDataAdapter);
     }
 
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        String item = parent.getItemAtPosition(pos).toString().toLowerCase();
-//        Log.d("itemSelected", item);
-        getData(item);
-    }
+//    public void onItemSelected(AdapterView<?> parent, View view,
+//                               int pos, long id) {
+//        // An item was selected. You can retrieve the selected item using
+//        String item = parent.getItemAtPosition(pos).toString().toLowerCase();
+////        Log.d("itemSelected", item);
+//        getFirstLevelData(item);
+//    }
 
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
-        final TextView textContent = findViewById(R.id.textContent);
-        textContent.setText("");
-    }
+//    public void onNothingSelected(AdapterView<?> parent) {
+//        // Another interface callback
+////        final TextView textContent = findViewById(R.id.textContent);
+////        textContent.setText("");
+//    }
 
-    private void getData(final String resource) {
+    private void getFirstLevelData(final String resource) {
 
         if (resource != "") {
-
-            final TextView textContent = findViewById(R.id.textContent);
-
-    //        final EditText editText = findViewById(R.id.editText);
-
-    //        final String resource = editText.getText().toString();
 
             JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url + resource, new Response.Listener<JSONArray>() {
                 @Override
@@ -104,7 +119,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     //                        property.setId(jsonObject.getInt("id"));
     //                        property.setImage_url(jsonObject.getString("image"));
 
-                            //propertyList.add(property);
+                            propertyList.add(jsonObject);
                         } catch (JSONException e) {
                             textContent.setText("Error");
                             Log.d("Test", "Calling FAB");
@@ -113,6 +128,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         }
                     }
 //                    adapter.notifyDataSetChanged();
+                    loadContentSpinner(propertyList);
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -124,6 +140,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             RequestQueue requestQueue = Volley.newRequestQueue(this);
             requestQueue.add(jsonArrayRequest);
         }
+    }
+
+    private void getSecondLevelData(final String resource, final String item_id) {
+
+        if (resource != "") {
+
+            Log.e("item_id", item_id);
+
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url + resource + "/" + item_id, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    textContent.setText(response.toString());
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Volley", error.toString());
+                    textContent.setText("Error");
+                }
+            });
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(jsonArrayRequest);
+        }
+    }
+
+    private void loadContentSpinner(List<JSONObject> propertyList) {
+        ArrayAdapter<JSONObject> contentDataAdapter = new ArrayAdapter<JSONObject>(this, android.R.layout.simple_spinner_item, propertyList);
+        contentDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        contentSpinner.setAdapter(contentDataAdapter);
+
     }
 }
 
